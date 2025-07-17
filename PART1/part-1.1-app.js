406a19a03fac518656a967321c17df5abc5e0132
Akshit <akshitpjt07@gmail.com>
2025-07-17 18:08:26 +0530

    PART-1

---
+++ PART1/part-1.1-app.js
@@ -0,0 +1,10 @@
+var name = "NodeJs"
+
+var a = 10
+
+var b = 20
+
+console.log(name)
+console.log(a+b)
+
+// how will i run this code -> go in terminal -> and hit -> node app.js [filename which has the code]
---
+++ PART1/part-1.2-app.js
@@ -0,0 +1,6 @@
+// What is global in node js as we have in browser global as window/this
+// In node js global object is "global" or "globalthis" (the standard convention -> globalThis)
+
+// console.log(global)
+console.log(globalThis)
+// console.log(this)
