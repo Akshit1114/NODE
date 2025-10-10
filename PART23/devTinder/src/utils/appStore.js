d941504b05a456a867d2ca7cdd98b65acdcc2423
Akshit <akshitpjt07@gmail.com>
2025-10-10 10:38:11 +0530

    PART23

---
+++ PART23/devTinder/src/utils/appStore.js
@@ -0,0 +1,15 @@
+
+import {configureStore} from '@reduxjs/toolkit'
+
+import userReducer from './userSlice'
+import feedReducer from './feedSlice'
+
+const appSotre = configureStore({
+    reducer : { 
+        user : userReducer,
+        feed : feedReducer
+     }
+})
+
+
+export default appSotre
